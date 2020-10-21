719a7f15afa9c7a25fea7aac986e8e8d9b13b5cc
yosheo <sean.defontaine@gmail.com>
2020-10-21 11:47:21 -0400

    Updated keys

--- MarketGateway.py
+++ MarketGateway.py
@@ -10,10 +10,8 @@ class MarketGateway:
     def __init__(self):
         # alpaca paper trading
         alpaca_url = "https://paper-api.alpaca.markets"
-        # alpaca_key = "YOUR-API-KEY"
-        alpaca_key = "PKVQE5XUH9W1VSDYEVCT"
-        alpaca_secret = "uDBDUQ2GOxJyj2tM1Q0pMZ0ZrcB4FU9xRAE6yKeF"
-        # alpaca_secret = "YOUR-SECRET-KEY"
+        alpaca_key = "YOUR-PUBLIC-KEY"
+        alpaca_secret = "YOUR-PRIVATE-KEY"
         self.alpaca = tradeapi.REST(alpaca_key,alpaca_secret,alpaca_url,'v2')
         print('Brokerage connection successful...')
 
